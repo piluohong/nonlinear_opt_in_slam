bb621bc5bb8c047d043c446884699d64f4caa0c7
piluohong <1912694135@qq.com>
2024-02-23 16:54:39 +0800

    update

--- src/mid360_image.cpp
+++ src/mid360_image.cpp
@@ -65,46 +65,82 @@ static pcl::PointCloud<PointType>::Ptr copy_depthCloud (new pcl::PointCloud<Poin
 static cv::Mat project_image;
 static Eigen::Matrix4f extrinsicMat_RT; // 外参旋转矩阵3*3和平移向量3*1
 static cv::Mat intrisicMat(3, 4, cv::DataType<double>::type);// 内参3*4的投影矩阵，最后一列是三个零
-static cv::Mat distCoeffs(3, 3, cv::DataType<double>::type);
+static cv::Mat intrisic(3, 3, cv::DataType<double>::type);			   // 内参3*3矩阵
+static cv::Mat distCoeffs(5, 1, cv::DataType<double>::type);// 畸变向量
 
 void CalibrationData()
 {
-    //hku1.bag -> config
-    extrinsicMat_RT(0, 0) = 0.00162756;
-	extrinsicMat_RT(0, 1) = -0.999991;
-	extrinsicMat_RT(0, 2) = 0.00390957;
-	extrinsicMat_RT(0, 3) = 0.0409257;
-	extrinsicMat_RT(1, 0) = -0.0126748;
-	extrinsicMat_RT(1, 1) = -0.00392989;
-	extrinsicMat_RT(1, 2) = -0.999912;
-	extrinsicMat_RT(1, 3) = 0.0318424;
-	extrinsicMat_RT(2, 0) = 0.999918;
-	extrinsicMat_RT(2, 1) = 0.00157786;
-	extrinsicMat_RT(2, 2) = -0.012681;
-	extrinsicMat_RT(2, 3) = -0.0927219;
+    //hku1.bag -> config lidar->cam
+    // extrinsicMat_RT(0, 0) = 0.00162756;
+	// extrinsicMat_RT(0, 1) = -0.999991;
+	// extrinsicMat_RT(0, 2) = 0.00390957;
+	// extrinsicMat_RT(0, 3) = 0.0409257;
+	// extrinsicMat_RT(1, 0) = -0.0126748;
+	// extrinsicMat_RT(1, 1) = -0.00392989;
+	// extrinsicMat_RT(1, 2) = -0.999912;
+	// extrinsicMat_RT(1, 3) = 0.0318424;
+	// extrinsicMat_RT(2, 0) = 0.999918;
+	// extrinsicMat_RT(2, 1) = 0.00157786;
+	// extrinsicMat_RT(2, 2) = -0.012681;
+	// extrinsicMat_RT(2, 3) = -0.0927219;
+	// extrinsicMat_RT(3, 0) = 0.0;
+	// extrinsicMat_RT(3, 1) = 0.0;
+	// extrinsicMat_RT(3, 2) = 0.0;
+	// extrinsicMat_RT(3, 3) = 1.0;
+
+    // intrisicMat.at<double>(0, 0) =  863.590518437255;
+	// intrisicMat.at<double>(0, 1) = 0.000000e+00;
+	// intrisicMat.at<double>(0, 2) =  621.666074631063;
+	// intrisicMat.at<double>(0, 3) = 0.000000e+00;
+	// intrisicMat.at<double>(1, 0) = 0.000000e+00;
+	// intrisicMat.at<double>(1, 1) =  863.100180533059;
+	// intrisicMat.at<double>(1, 2)  = 533.971978652819;
+	// intrisicMat.at<double>(1, 3) = 0.000000e+00;
+	// intrisicMat.at<double>(2, 0) = 0.000000e+00;
+	// intrisicMat.at<double>(2, 1) = 0.000000e+00;
+	// intrisicMat.at<double>(2, 2) = 1.000000e+00;
+	// intrisicMat.at<double>(2, 3) = 0.000000e+00;
+
+    //hku_main_building -> config cam->lidar
+    extrinsicMat_RT(0, 0) = -0.00113207;
+	extrinsicMat_RT(0, 1) = -0.0158688;
+	extrinsicMat_RT(0, 2) = 0.999873;
+	extrinsicMat_RT(0, 3) = 0.050166;
+	extrinsicMat_RT(1, 0) = -0.9999999;
+	extrinsicMat_RT(1, 1) = -0.000486594;
+	extrinsicMat_RT(1, 2) = -0.00113994;
+	extrinsicMat_RT(1, 3) = 0.0474116;
+	extrinsicMat_RT(2, 0) = 0.000504622;
+	extrinsicMat_RT(2, 1) = -0.999874;
+	extrinsicMat_RT(2, 2) = -0.0158682;
+	extrinsicMat_RT(2, 3) = -0.0312415;
 	extrinsicMat_RT(3, 0) = 0.0;
 	extrinsicMat_RT(3, 1) = 0.0;
 	extrinsicMat_RT(3, 2) = 0.0;
 	extrinsicMat_RT(3, 3) = 1.0;
 
-    intrisicMat.at<double>(0, 0) =  863.590518437255;
-	intrisicMat.at<double>(0, 1) = 0.000000e+00;
-	intrisicMat.at<double>(0, 2) =  621.666074631063;
+    intrisicMat.at<double>(0, 0) = intrisic.at<double>(0,0)=  863.4241;
+	intrisicMat.at<double>(0, 1) = intrisic.at<double>(0,1)= 0.000000e+00;
+	intrisicMat.at<double>(0, 2) = intrisic.at<double>(0,2)=  640.6808;
 	intrisicMat.at<double>(0, 3) = 0.000000e+00;
-	intrisicMat.at<double>(1, 0) = 0.000000e+00;
-	intrisicMat.at<double>(1, 1) =  863.100180533059;
-	intrisicMat.at<double>(1, 2)  = 533.971978652819;
+	intrisicMat.at<double>(1, 0) = intrisic.at<double>(1,0)= 0.000000e+00;
+	intrisicMat.at<double>(1, 1) = intrisic.at<double>(1,1)=  863.4171;
+	intrisicMat.at<double>(1, 2) = intrisic.at<double>(1,2)= 518.3392;
 	intrisicMat.at<double>(1, 3) = 0.000000e+00;
-	intrisicMat.at<double>(2, 0) = 0.000000e+00;
-	intrisicMat.at<double>(2, 1) = 0.000000e+00;
-	intrisicMat.at<double>(2, 2) = 1.000000e+00;
+	intrisicMat.at<double>(2, 0) = intrisic.at<double>(2,0)= 0.000000e+00;
+	intrisicMat.at<double>(2, 1) = intrisic.at<double>(2,1)= 0.000000e+00;
+	intrisicMat.at<double>(2, 2) = intrisic.at<double>(2,2)= 1.000000e+00;
 	intrisicMat.at<double>(2, 3) = 0.000000e+00;
+    
+    // std::cout << intrisic << std::endl;
 
-	// distCoeffs.at<double>(0) = -0.008326874784366894;
-	// distCoeffs.at<double>(1) = -0.06967846599874981;
-	// distCoeffs.at<double>(2) = 0.006185220615585947;
-	// distCoeffs.at<double>(3) = -0.01133018681519818;
-	// distCoeffs.at<double>(4) = 0.5462976722456516;
+    //k1,k2,p1,p2,k3
+    distCoeffs.at<double>(0) = -0.1080;
+    distCoeffs.at<double>(1) = 0.1050;
+    distCoeffs.at<double>(2) = -1.2872e-04;
+    distCoeffs.at<double>(3) = 5.7923e-05;
+    distCoeffs.at<double>(4) = -0.0222;
+    
 
 }
 
@@ -169,12 +205,18 @@ void ImageCallback(const sensor_msgs::ImageConstPtr &image_msg)
 
     if (abs(cur_time - cur_laser_time) > 0.1)
         return;
-
-    cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
+    // ROS_Image -> OpenCV_Image
+    cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::RGB8);
     cv::Mat cv_image;
     cv_image = cv_ptr->image;
-    cv::Mat circle_image(1024, 1280, CV_8UC3, cv::Scalar(255,255,255));
 
+    //图片去畸变
+    cv::Mat map1, map2;
+    cv::Size imageSize = cv_image.size();
+    cv::initUndistortRectifyMap(intrisic, distCoeffs, cv::Mat(), cv::getOptimalNewCameraMatrix(intrisic, distCoeffs, imageSize, 1, imageSize, 0), imageSize, CV_16SC2, map1, map2);
+    cv::remap(cv_image, cv_image, map1, map2, cv::INTER_LINEAR); // correct the distortion
+
+    cv::Mat circle_image(1024, 1280, CV_8UC3, cv::Scalar(255,255,255));
     circle_image = cv_image.clone();
     //原始图像缩放0.5倍
     // double scale_factor = 0.5;
@@ -230,26 +272,24 @@ void ImageCallback(const sensor_msgs::ImageConstPtr &image_msg)
     // cv::addWeighted(cv_image, 1.0,circle_image, 0.5, 0, cv_image);
     cv_bridge::CvImage bridge;
     bridge.image = circle_image;
-    bridge.encoding = "bgr8";
+    bridge.encoding = "rgb8";
     sensor_msgs::Image::Ptr imageShowPointer = bridge.toImageMsg();
     imageShowPointer->header.stamp = ros::Time::now();
     pub_depth_image.publish(imageShowPointer);
 
     colorCloud->width = colorCloud->points.size();
     colorCloud->height = 1;
-    ROS_WARN(">>> %d\n",colorCloud->width);
+    ROS_WARN(">>> %d\n",int(copy_depthCloud->points.size() - colorCloud->width));
     sensor_msgs::PointCloud2 color_msg;
     pcl::toROSMsg(*colorCloud, color_msg);			   
-    color_msg.header.frame_id = "camera_link";		   
+    color_msg.header.frame_id = image_msg->header.frame_id;		   
     color_msg.header.stamp = ros::Time::now();
     pubCloud.publish(color_msg);
 
     colorCloud->clear();
 
     // cv::imwrite("/home/hong/slam/hhh_ws/src/nonlinear_opt_in_slam/data/saveRangeImageRGB.png",circle_image);
-
     // ROS_INFO("*****************************************\n");
-    // ROS_WARN("*****************************************\n");
 
     return;
 }
@@ -269,7 +309,8 @@ void LivoxCallback(const livox_ros_driver2::CustomMsgConstPtr& cloud_msg)
         /* code */
         listener.waitForTransform("world", "livox_frame", ros::Time::now(), ros::Duration(0.01));
         listener.lookupTransform("world", "livox_frame", ros::Time::now(), transform);
-    }catch(tf::TransformException ex)
+    }
+    catch(tf::TransformException ex)
     {
         std::cerr << "lidar no tf to world." << '\n';
         return;
@@ -289,7 +330,7 @@ void LivoxCallback(const livox_ros_driver2::CustomMsgConstPtr& cloud_msg)
     // 提取点云并降采样
     pcl::PointCloud<PointType>::Ptr laser_cloud(new pcl::PointCloud<PointType>());
     LivoxMsgToPcl(msg,laser_cloud);
-    pcl::PointCloud<PointType>::Ptr laser_cloud_DS (new pcl::PointCloud<PointType>());
+    // pcl::PointCloud<PointType>::Ptr laser_cloud_DS (new pcl::PointCloud<PointType>());
     static pcl::VoxelGrid<PointType> downSizeFilter;
     // downSizeFilter.setLeafSize(0.2,0.2,0.2);
     // downSizeFilter.setInputCloud(laser_cloud);
@@ -314,6 +355,7 @@ void LivoxCallback(const livox_ros_driver2::CustomMsgConstPtr& cloud_msg)
     Eigen::Affine3f transOffset;
     transOffset.linear() = linearPart;
     transOffset.translation() = translation;
+    transOffset = transOffset.inverse();
     pcl::transformPointCloud(*laser_cloud,*laser_cloud_offset,transOffset);
     *laser_cloud = *laser_cloud_offset;
 
@@ -322,29 +364,28 @@ void LivoxCallback(const livox_ros_driver2::CustomMsgConstPtr& cloud_msg)
     pcl::transformPointCloud(*laser_cloud,*laser_cloud_global,transNow);
     *depthCloud = *laser_cloud_global;
 
-    // 保存转换后的新点云
-    
+    // 保存新的点云
     double timeScanCur = msg->header.stamp.toSec();
-    timeQueue.push_back(timeScanCur);
-    cloudQueue.push_back(*laser_cloud_global);
+    // timeQueue.push_back(timeScanCur);
+    // cloudQueue.push_back(*laser_cloud_global);
 
-    //累计1s内的点云
-    while (!timeQueue.empty())
-    {
-        /* code */
-        if (timeScanCur - timeQueue.front() > 1)
-        {
-            cloudQueue.pop_front();
-            timeQueue.pop_front();
-        }else{
-            break;
-        }
-    }
-    depthCloud->clear();
+    // //累计1s内的点云
+    // while (!timeQueue.empty())
+    // {
+    //     /* code */
+    //     if (timeScanCur - timeQueue.front() > 1)
+    //     {
+    //         cloudQueue.pop_front();
+    //         timeQueue.pop_front();
+    //     }else{
+    //         break;
+    //     }
+    // }
+    // depthCloud->clear();
     std::lock_guard<std::mutex> lock(mtx_lidar);
     laser_time = timeScanCur;
-    for (int i = 0; i < (int)cloudQueue.size(); ++i)
-        *depthCloud += cloudQueue[i];
+    // for (int i = 0; i < (int)cloudQueue.size(); ++i)
+    //     *depthCloud += cloudQueue[i];
    
     // pcl::PointCloud<PointType>::Ptr depthCloudDS(new pcl::PointCloud<PointType>());
     // downSizeFilter.setLeafSize(0.2, 0.2, 0.2);
